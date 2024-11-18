5f0c4d3a33ea37f91a83863cdb76e977ec701f78
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-18 17:30:51 +0200

    add: AppRoutes & RoutesList components - react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>;
-}
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
 
 export default App;
