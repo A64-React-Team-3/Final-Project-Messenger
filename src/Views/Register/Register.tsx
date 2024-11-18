5b06588aa66d4919b9cba9b3f6e5dadf0ef2e049
DongCuong <geas0621@gmail.com>
2024-11-18 21:03:50 +0200

    added: Register view update: Anonymous view to show Login and Register with "To Register", "Back to Login" buttons update: Login to show Register view with "To Register" button

--- src/Views/Register/Register.tsx
+++ src/Views/Register/Register.tsx
@@ -1,11 +1,11 @@
-import './Login.css';
+import './Register.css';
 
-export default function Login({ handleShowRegister }: { handleShowRegister: () => void }) {
+export default function Register({ handleShowLogin }: { handleShowLogin: () => void }) {
 
   return (
     <div className="card backdrop-opacity backdrop-invert bg-white/40 shadow-2xl shadow-blue-100/50 w-96">
       <div className="card-body">
-        <h2 className="card-title">Login</h2>
+        <h2 className="card-title">Register</h2>
         <label className="input input-bordered flex items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -26,18 +26,41 @@ export default function Login({ handleShowRegister }: { handleShowRegister: () =
             fill="currentColor"
             className="h-4 w-4 opacity-70">
             <path
+              d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+          </svg>
+          <input type="text" className="grow" placeholder="Username" />
+        </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
               fillRule="evenodd"
               d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
               clipRule="evenodd" />
           </svg>
           <input type="password" className="grow" value="" placeholder='password' />
         </label>
+        <label className="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="h-4 w-4 opacity-70">
+            <path
+              fillRule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clipRule="evenodd" />
+          </svg>
+          <input type="password" className="grow" value="" placeholder='confirm password' />
+        </label>
         <div className="card-actions justify-end">
-          <button className="btn btn-accent opacity-50">Login</button>
-          <button className="btn btn-info opacity-50" onClick={handleShowRegister}>To Register</button>
+          <button className="btn btn-accent opacity-50" onClick={handleShowLogin}>Back to Login</button>
+          <button className="btn btn-info opacity-50">Register</button>
         </div>
       </div>
     </div>
-
-  );
+  )
 }
