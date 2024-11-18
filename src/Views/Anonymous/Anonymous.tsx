e9b819ebbc191b3ab037b77c622060b2aa53fe56
DongCuong <geas0621@gmail.com>
2024-11-18 17:06:13 +0200

    added Login component with initial css

--- src/Views/Anonymous/Anonymous.tsx
+++ src/Views/Anonymous/Anonymous.tsx
@@ -1,10 +1,16 @@
+import Login from "../Login/Login";
 import "./Anonymous.css";
+import { useState } from "react";
 
 export default function Anonymous() {
+  const [showLogin, setShowLogin] = useState(true);
+
   return (
     <div className="main-anonymous-view">
       <div className="app-info">Some info</div>
-      <div className="login-register-form">login&register</div>
+      <div className="login-register-form">
+        <Login />
+      </div>
     </div>
   );
 };
