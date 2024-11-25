29c50ebaa26e0d9466f14923199a720f6be39e32
DongCuong <geas0621@gmail.com>
2024-11-25 19:56:32 +0200

    update: added placeholder collapse Text Channels and Voice Channels

--- src/components/TeamSideBar/TeamSideBar.tsx
+++ src/components/TeamSideBar/TeamSideBar.tsx
@@ -1,6 +1,27 @@
 const TeamSideBar: React.FC = (): JSX.Element => {
   return (
-    <div className="border-base-300 flex justify-center px-4 py-16 bg-slate-600 text-slate-50 h-full w-60">Channel list</div>
+    <div className="border-base-300 flex-col justify-center px-4 bg-slate-600 text-slate-50 h-full w-60">
+      <div className="collapse">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">Text Channels</div>
+        <div className="collapse-content">
+          <p>Channel 1</p>
+          <p>Channel 2</p>
+          <p>Channel 3</p>
+          <p>Channel 4</p>
+        </div>
+      </div>
+      <div className="collapse">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">Voice Channels</div>
+        <div className="collapse-content">
+          <p>Channel 1</p>
+          <p>Channel 2</p>
+          <p>Channel 3</p>
+          <p>Channel 4</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
