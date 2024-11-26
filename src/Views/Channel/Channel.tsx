4022e429c29ec568060bd3cad6a3cbc1331882f0
DongCuong <geas0621@gmail.com>
2024-11-26 17:48:05 +0200

    created channel view

---
+++ src/Views/Channel/Channel.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+
+const Channel: React.FC = (): JSX.Element => {
+
+
+  return (
+    <div className="channel-view flex bg-slate-500 w-[calc(100vw-20rem)]">
+      <p>Channel</p>
+    </div>
+  );
+}
+
+export default Channel;
--- src/Views/Home/Home.tsx
+++ src/Views/Home/Home.tsx
@@ -1,7 +1,4 @@
 import React, { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { UserAppContext } from "../../store/app-context";
-import { signOutUser } from "../../services/auth.service";
 import "./Home.css";
 import HomeSideBar from "../../components/HomeSideBar/HomeSideBar";
 import Team from "../Team/Team";
