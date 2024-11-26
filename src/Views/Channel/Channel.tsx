a3d995c245ec2d26bed64a68cbcc476435358a9c
DongCuong <geas0621@gmail.com>
2024-11-26 22:03:42 +0200

    created: Channel view, Messages and ChannelSideBar components

--- src/Views/Channel/Channel.tsx
+++ src/Views/Channel/Channel.tsx
@@ -1,14 +1,75 @@
-
-
+import ChannelSideBar from "../../components/ChannelSideBar/ChannelSideBar";
+import { useEffect, useRef, useState } from "react";
+import Message from "../../components/Message/Message";
 
 
 
 const Channel: React.FC = (): JSX.Element => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const chatRef = useRef<HTMLDivElement>(null);
+  const [message, setMessage] = useState<string>('');
+  const [messages, setMessages] = useState<string[]>([]);
+  const [textareaHeight, setTextareaHeight] = useState(0);
+
+  const sendMessage = () => {
+    if (message.trim() === '') return;
+    setMessages([...messages, message]);
+    setMessage('');
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
+
+  const handleInput = () => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = 'auto';
+      const newHeight = textareaRef.current.scrollHeight;
+      textareaRef.current.style.height = `${newHeight}px`;
+    }
+  };
+
+  useEffect(() => {
+    console.log('Message:', message);
+    console.log('Messages:', messages);
+  }, [message, messages]);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      setTextareaHeight(textareaRef.current.scrollHeight);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [messages]);
 
 
   return (
     <div className="channel-view flex bg-slate-500 w-[calc(100vw-20rem)]">
-      <p>Channel</p>
+      <div className="bg-red-400 flex flex-col w-[calc(100vw-35rem)] h-full">
+        <div ref={chatRef} className="display-chat bg-slate-900 w-[calc(100vw-35rem)] flex-grow overflow-auto p-2">
+          {messages.map((msg, idx) => (
+            <Message key={idx} message={msg} />
+          ))}
+        </div>
+        <textarea
+          ref={textareaRef}
+          className="w-full p-2 text-black resize-none overflow-hidden"
+          placeholder="Type your message here... and press Enter to send"
+          onInput={handleInput}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={handleKeyDown}
+          style={{ height: `${textareaHeight}px`, minHeight: '5rem' }}
+        ></textarea>
+      </div>
+      <ChannelSideBar />
     </div>
   );
 }
---
+++ src/components/Message/Message.tsx
@@ -0,0 +1,21 @@
+type MessageProps = {
+  message: string;
+};
+
+
+const Message: React.FC<MessageProps> = ({ message }): JSX.Element => {
+  return (
+    <div className="chat chat-start">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img
+            alt="Tailwind CSS chat bubble component"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+        </div>
+      </div>
+      <div className="chat-bubble break-words max-w-2xl">{message}</div>
+    </div>
+  );
+};
+
+export default Message;
