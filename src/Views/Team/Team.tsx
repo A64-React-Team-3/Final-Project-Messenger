6ea16dc0e5247087d06e5d3b994aacc5c0eb24aa
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-28 19:03:08 +0200

    add: ProfileButton extracted from TeamNavBar into own component for reusability

--- src/Views/Team/Team.tsx
+++ src/Views/Team/Team.tsx
@@ -1,7 +1,4 @@
-import { signOutUser } from "../../services/auth.service";
-import { useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
-import { UserAppContext } from "../../store/user.context.ts";
+import { useEffect } from "react";
 import TeamNavBar from "../../components/TeamNavBar/TeamNavBar";
 import TeamSideBar from "../../components/TeamSideBar/TeamSideBar";
 import Channel from "../Channel/Channel";
@@ -11,24 +8,8 @@ import { db } from "../../config/firebase-config";
 import { transformChannels } from "../../helper/helper";
 
 const Team: React.FC = (): JSX.Element => {
-  const navigate = useNavigate();
-  const { user, setUser } = useContext(UserAppContext);
   const [channels, setChannels] = useState<any[]>([]);
   const [channel, setChannel] = useState<any>(null);
-
-  const handleLogout = async () => {
-    try {
-      await signOutUser();
-      setUser(null);
-      navigate("/", { replace: true });
-    } catch (err: any) {
-      console.log(err);
-    }
-  };
-
-  const handleToSettings = () => {
-    navigate("/settings");
-  };
 
   useEffect(() => {
     const channelsRef = ref(db, "channels/");
@@ -47,11 +28,7 @@ const Team: React.FC = (): JSX.Element => {
 
   return (
     <div className="border-base-300 flex-col justify-center w-full bg-slate-500 text-white">
-      <TeamNavBar
-        handleLogout={handleLogout}
-        handleToSettings={handleToSettings}
-        channelName={channel?.name}
-      />
+      <TeamNavBar channelName={channel?.name} />
       <div className="flex w-full h-[calc(100vh-4rem)]">
         <TeamSideBar channels={channels} setChannel={setChannel} />
         <Channel channel={channel} />
