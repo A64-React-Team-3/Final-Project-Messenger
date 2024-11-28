5cc2139233b9aa7f1f1179f6237f7fcbd403121d
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-28 18:08:15 +0200

    update: imports of user.context

--- src/Views/Team/Team.tsx
+++ src/Views/Team/Team.tsx
@@ -1,7 +1,7 @@
 import { signOutUser } from "../../services/auth.service";
 import { useNavigate } from "react-router-dom";
 import { useContext, useEffect } from "react";
-import { UserAppContext } from "../../store/app-context";
+import { UserAppContext } from "../../store/user.context.ts";
 import TeamNavBar from "../../components/TeamNavBar/TeamNavBar";
 import TeamSideBar from "../../components/TeamSideBar/TeamSideBar";
 import Channel from "../Channel/Channel";
@@ -33,7 +33,7 @@ const Team: React.FC = (): JSX.Element => {
   useEffect(() => {
     const channelsRef = ref(db, "channels/");
     if (channelsRef) {
-      const unsubscribe = onValue(channelsRef, (snapshot) => {
+      const unsubscribe = onValue(channelsRef, snapshot => {
         const transformedData = transformChannels(snapshot);
         if (transformedData) {
           const channels = Object.values(transformedData);
@@ -45,16 +45,19 @@ const Team: React.FC = (): JSX.Element => {
     }
   }, []);
 
-
   return (
     <div className="border-base-300 flex-col justify-center w-full bg-slate-500 text-white">
-      <TeamNavBar handleLogout={handleLogout} handleToSettings={handleToSettings} channelName={channel?.name} />
+      <TeamNavBar
+        handleLogout={handleLogout}
+        handleToSettings={handleToSettings}
+        channelName={channel?.name}
+      />
       <div className="flex w-full h-[calc(100vh-4rem)]">
         <TeamSideBar channels={channels} setChannel={setChannel} />
         <Channel channel={channel} />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Team;
