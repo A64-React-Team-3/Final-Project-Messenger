3ab41d90b4f2dfe77ab7c2b018883b74d345c973
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-28 21:06:33 +0200

    update: HomeSideBar lifted onClick event to wrapping div

--- src/components/HomeSideBar/HomeSideBar.tsx
+++ src/components/HomeSideBar/HomeSideBar.tsx
@@ -12,9 +12,9 @@ const HomeSideBar: React.FC = (): JSX.Element => {
   return (
     <div className="border-base-300 flex-row justify-center px-4 bg-black text-slate-50 w-20">
       <div className="personal-team flex-row pt-2 mb-4 border-b-2 h-16">
-        <div className="avatar placeholder">
+        <div className="avatar placeholder" onClick={handleToPersonal}>
           <div className="bg-neutral btn btn-success text-neutral-content w-12 rounded-full border-none">
-            <span onClick={handleToPersonal}>DM's</span>
+            <span>DM's</span>
           </div>
         </div>
       </div>
