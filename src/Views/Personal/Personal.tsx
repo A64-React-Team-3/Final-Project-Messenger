9e8760a38424f8b80350b3dfc5bf378575508db3
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-28 19:03:58 +0200

    add: Personal view component with a base layout

--- src/Views/Personal/Personal.tsx
+++ src/Views/Personal/Personal.tsx
@@ -1,4 +1,49 @@
+import HomeSideBar from "../../components/HomeSideBar/HomeSideBar";
+import ProfileButton from "../../components/ProfileButton/ProfileButton";
+
 const Personal: React.FC = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex w-full h-[calc(100vh-4rem)]">
+        <HomeSideBar />
+        <div className="border-base-300 flex-col justify-center px-4 bg-slate-600 text-slate-50 h-full w-60">
+          <div className="collapse">
+            <input type="checkbox" />
+            <div className="collapse-title text-xl font-medium">
+              Friend Requests
+            </div>
+            <div className="collapse-content">
+              <p>User 1</p>
+              <p>User 2</p>
+              <p>User 3</p>
+            </div>
+            <div className="collapse">
+              <input type="checkbox" />
+              <div className="collapse-title text-xl font-medium">Friends</div>
+              <div className="collapse-content">
+                <p>User 1</p>
+                <p>User 2</p>
+                <p>User 3</p>
+              </div>
+              <div className="collapse">
+                <input type="checkbox" />
+                <div className="collapse-title z-0 text-xl font-medium">
+                  Text Channels
+                </div>
+                <div className="collapse-content">
+                  <p>Channel 1</p>
+                  <p>Channel 2</p>
+                  <p>Channel 3</p>
+                  <p>Channel 4</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="navbar py-0 px-0 bg-zinc-900 h-14"></div>
+        <ProfileButton />
+      </div>
+    </>
+  );
 };
 export default Personal;
