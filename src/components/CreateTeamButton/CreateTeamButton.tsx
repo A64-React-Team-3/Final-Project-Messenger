3fe8dda617ad0109f38ecf7f42bfec7c2469b9c6
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-29 17:46:28 +0200

    add: CreateTeamButton component

---
+++ src/components/CreateTeamButton/CreateTeamButton.tsx
@@ -0,0 +1,10 @@
+const CreateTeamButton: React.FC = (): JSX.Element => {
+  return (
+    <div className="avatar placeholder">
+      <div className="mb-2 bg-neutral btn btn-secondary text-neutral-content w-12 rounded-full border-none">
+        <span>➕</span>
+      </div>
+    </div>
+  );
+};
+export default CreateTeamButton;
