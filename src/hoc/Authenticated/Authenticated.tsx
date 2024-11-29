ee9f6ae815d09d1176ce3e840e7614fb9b5e3cf3
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-29 16:53:50 +0200

    fix: URL redirect issue with Anonymous
    useEffect now checks for authUser from useAuthState hook

--- src/hoc/Authenticated/Authenticated.tsx
+++ src/hoc/Authenticated/Authenticated.tsx
@@ -1,9 +1,9 @@
-import React, { useContext, useEffect } from "react";
-import { UserAppContext } from "../../store/user.context";
+import React, { useEffect } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../config/firebase-config";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 type AuthenticatedProps = {
   /** React components to render if the user is authenticated */
@@ -22,7 +22,6 @@ type AuthenticatedProps = {
 const Authenticated: React.FC<AuthenticatedProps> = ({
   children,
 }: AuthenticatedProps): JSX.Element => {
-  const { user } = useContext(UserAppContext);
   const navigate = useNavigate();
   const [authUser, loading] = useAuthState(auth);
 
@@ -35,14 +34,11 @@ const Authenticated: React.FC<AuthenticatedProps> = ({
       }
     }
   }, [authUser, loading]);
-  if (loading) {
-    return (
-      <>
-        <h2>loading....</h2>
-      </>
-    );
+  if (authUser) {
+    return <div>{children}</div>;
+  } else {
+    return <LoadingSpinner />;
   }
-  return <div>{children}</div>;
 };
 
 export default Authenticated;
