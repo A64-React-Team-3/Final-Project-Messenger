0a8327aa792cae084df51add15138756bda606ec
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-11-29 16:53:50 +0200

    fix: URL redirect issue with Anonymous
    useEffect now checks for authUser from useAuthState hook

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Authenticated from "../hoc/Authenticated/Authenticated";
 import Anonymous from "../Views/Anonymous/Anonymous";
 import Home from "../Views/Home/Home";
 import SettingsPage from "../Views/Settings/SettingsPage/SettingsPage";
 import Personal from "../Views/Personal/Personal";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../config/firebase-config";
 
 /**
  * Renders application routes.
  * @returns {JSX.Element} The rendered routes.
  */
 export const AppRoutes: React.FC = (): JSX.Element => {
+  const [authUser] = useAuthState(auth);
   return (
     <Routes>
-      <Route path="/" element={<Anonymous />} />
+      <Route
+        path="/"
+        element={
+          <>{authUser ? <Navigate to="/home" replace /> : <Anonymous />}</>
+        }
+      />
       <Route
         path="/home"
         element={
