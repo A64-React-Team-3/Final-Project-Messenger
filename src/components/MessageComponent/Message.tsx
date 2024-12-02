861d098b475b00f863529428d577dddbb3addbeb
DongCuong <geas0621@gmail.com>
2024-12-02 17:22:19 +0200

    update: display reactions

--- src/components/MessageComponent/Message.tsx
+++ src/components/MessageComponent/Message.tsx
@@ -26,33 +26,41 @@ const Message: React.FC<MessageProps> = ({ message }): JSX.Element => {
   }, [message])
 
   return (
-    <div className={`chat ${message.sender === user!.uid ? "chat-end" : "chat-start"} my-5 p-0 relative`} onMouseEnter={() => setIsPickerVisible(true)} onMouseLeave={() => setIsPickerVisible(false)}>
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+    <>
+      <div className={`chat ${message.sender === user!.uid ? "chat-end" : "chat-start"} mt-5 mb-2 p-0 relative`} onMouseEnter={() => setIsPickerVisible(true)} onMouseLeave={() => setIsPickerVisible(false)}>
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
         </div>
+        <div className={`chat-header flex gap-1 items-center ${message.sender === user!.uid ? "flex-row-reverse" : ""}`}>
+          <span>{message.senderName}</span>
+          <time className="text-xs opacity-50"> {transformDate(message.timestamp)}</time>
+        </div>
+        <div className={`chat-bubble ${message.sender === user!.uid ? "chat-bubble-primary" : ""} break-words max-w-full`} >
+          <p className="break-words">
+            {message.message}
+          </p>
+        </div>
+        {isPickerVisible && <div className={`absolute z-10 top-[-2rem] ${message.sender === user!.uid ? "left-[50rem]" : "right-[50rem]"}`} style={{ transform: 'scale(0.85)' }}>
+          <EmojiPicker
+            onReactionClick={handleEmojiReactionClick}
+            onEmojiClick={handleEmojiReactionClick}
+            reactionsDefaultOpen={true}
+            lazyLoadEmojis={true}
+            searchDisabled={true}
+            skinTonesDisabled={true}
+          />
+        </div>}
       </div>
-      <div className={`chat-header flex gap-1 items-center ${message.sender === user!.uid ? "flex-row-reverse" : ""}`}>
-        <span>{message.senderName}</span>
-        <time className="text-xs opacity-50"> {transformDate(message.timestamp)}</time>
-      </div>
-      <div className={`chat-bubble ${message.sender === user!.uid ? "chat-bubble-primary" : ""} break-words max-w-full`} >
-        <p className="break-words">
-          {message.message}
-        </p>
-      </div>
-      {isPickerVisible && <div className={`absolute z-10 top-[-1rem] ${message.sender === user!.uid ? "left-[50rem]" : "right-[50rem]"}`} style={{ transform: 'scale(0.6)' }}>
-        <EmojiPicker
-          onReactionClick={handleEmojiReactionClick}
-          reactionsDefaultOpen={true}
-          lazyLoadEmojis={true}
-          searchDisabled={true}
-          skinTonesDisabled={true}
-        />
-      </div>}
-    </div>
+      <div className={`flex gap-2 z-10 bottom-[-1rem] w-50 flex-wrap ${message.sender === user!.uid ? "left-[70rem] flex-row-reverse" : "right-[70rem]"}`}>{message.reactions?.map(
+        (reaction, index) => (
+          <span key={index} className="text-xs">{reaction.name} {reaction.count}</span>
+        )
+      )}</div>
+    </>
   );
 };
 
--- src/models/MessageModel.ts
+++ src/models/MessageModel.ts
@@ -8,25 +8,9 @@ export type MessageModel = {
   imageUrl?: string;
   reactions?: [
     {
-      emojiName: string;
+      name: string;
       count: number;
       reactors: string[];
     }
   ];
 };
-
-
-// export type MessageModel = {
-//   message: string;
-//   senderName: string;
-//   sender: string;
-//   channelId: string;
-//   timestamp: number;
-//   imageUrl?: string;
-//   reactions?: {
-//     [emoji: string]: {
-//       count: number;
-//       reactors: string[];
-//     };
-//   };
-// };
