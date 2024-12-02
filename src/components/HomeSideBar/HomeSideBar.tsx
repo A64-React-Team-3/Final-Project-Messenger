e2937c445861baad81c82ceb472baff4eb734119
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-12-02 22:51:22 +0200

    update: HomeSideBar - renders teams dynamically

--- src/components/HomeSideBar/HomeSideBar.tsx
+++ src/components/HomeSideBar/HomeSideBar.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
 import TeamAvatarButton from "../TeamAvatarButton/TeamAvatarButton";
 import CreateTeamButton from "../CreateTeamButton/CreateTeamButton";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CreateTeam from "../../Views/ModalViews/CreateTeam/CreateTeam";
-
+import { get, onValue, ref } from "firebase/database";
+import { db } from "../../config/firebase-config";
+// import { TeamModel } from "../../helper/helper";
 const HomeSideBar: React.FC = (): JSX.Element => {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [teams, setTeams] = useState<any[] | null>(null);
 
   const handleToPersonal = () => {
     navigate("/dms");
@@ -17,9 +20,28 @@ const HomeSideBar: React.FC = (): JSX.Element => {
   const handleCreateTeam = () => {
     setOpenModal(prevValue => !prevValue);
   };
+  useEffect(() => {
+    const teamsRef = ref(db, "teams/");
+    get(teamsRef)
+      .then(snapshot => {
+        if (snapshot.exists()) {
+          const unsubscribe = onValue(teamsRef, snapshot => {
+            console.log(snapshot.val());
+            const teamsData = Object.values(snapshot.val());
+            console.log(teamsData);
+            setTeams(teamsData);
+          });
+
+          return () => unsubscribe();
+        }
+      })
+      .catch(error => {
+        console.error("Error getting channels", error);
+      });
+  }, []);
   return (
     <>
-      <div className="border-base-300 flex-row justify-center px-4 bg-black text-slate-50 w-20">
+      <div className="border-base-300 flex-row justify-center  bg-black text-slate-50 w-20 ml-4">
         <div className="personal-team flex-row pt-2 mb-4 border-b-2 h-16">
           <div className="avatar placeholder" onClick={handleToPersonal}>
             <div className="bg-neutral btn btn-success text-neutral-content w-12 rounded-full border-none">
@@ -27,19 +49,15 @@ const HomeSideBar: React.FC = (): JSX.Element => {
             </div>
           </div>
         </div>
-        <div className="list-of-teams flex-row space-y-2 pt-2">
-          <span onClick={handleToTeam}>
-            <TeamAvatarButton />
-          </span>
-          <span onClick={handleToTeam}>
-            <TeamAvatarButton />
-          </span>
-          <span onClick={handleToTeam}>
-            <TeamAvatarButton />
-          </span>
-          <span onClick={handleToTeam}>
-            <TeamAvatarButton />
-          </span>
+        <div className="list-of-teams flex-row space-y-2 pt-2 overflow-y-auto max-h-[calc(100vh-120px)]">
+          {teams &&
+            teams.map(teamData => {
+              return (
+                <span key={teamData.id} onClick={handleToTeam}>
+                  <TeamAvatarButton teamData={teamData} />
+                </span>
+              );
+            })}
           <span onClick={handleCreateTeam}>
             <CreateTeamButton />
           </span>
