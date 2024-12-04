c63bf1e402c76da4504152bb9a954ba2ba5c327f
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-12-04 19:07:46 +0200

    update: CreateTeam - loading spinner add

--- src/Views/ModalViews/CreateTeam/CreateTeam.tsx
+++ src/Views/ModalViews/CreateTeam/CreateTeam.tsx
@@ -6,10 +6,15 @@ import { toast } from "react-toastify";
 import { defaultTeamImgUrl } from "../../../common/constants.ts";
 type CreateTeamProps = {
   closeModal: () => void;
+  setLoading: (val: boolean) => void;
 };
-const CreateTeam: React.FC<CreateTeamProps> = ({ closeModal }): JSX.Element => {
+const CreateTeam: React.FC<CreateTeamProps> = ({
+  closeModal,
+  setLoading,
+}): JSX.Element => {
   const [isTeamPrivate, setIsTeamPrivate] = useState(false);
   const [teamName, setTeamName] = useState<string>("");
+
   const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
   const [avatarFile, setAvatarFile] = useState<File | null>(null);
   const { user } = useContext(UserAppContext);
@@ -38,6 +43,8 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ closeModal }): JSX.Element => {
     const privacy = isTeamPrivate ? "private" : "public";
     let imageUrl = defaultTeamImgUrl;
     if (avatarFile) {
+      setLoading(true);
+      closeModal();
       try {
         imageUrl = await uploadImage(avatarFile);
         const teamData = {
@@ -52,72 +59,75 @@ const CreateTeam: React.FC<CreateTeamProps> = ({ closeModal }): JSX.Element => {
     }
     try {
       await createTeam(user!, teamName, privacy, imageUrl);
-      closeModal();
+      setLoading(false);
     } catch (error) {
       console.error("Error with creating team: ", error);
+      setLoading(false);
     }
   };
 
   return (
-    <div className="modal modal-open">
-      <div className="modal-box shadow-xl rounded-lg">
-        <h3 className="font-bold text-xl mb-4 text-center">
-          Create a new Team:
-        </h3>
-        <div className="space-y-4 flex flex-col items-center">
-          <input
-            type="text"
-            value={teamName}
-            onChange={updateTeamName}
-            placeholder="Enter team name"
-            className="input  input-bordered input-primary w-3/4 input-sm"
-          />
-          <div className="flex justify-evenly items-center">
-            <label className="label cursor-pointer">
-              <span className="label-text text-white">Private</span>
-              <input
-                type="checkbox"
-                name="teamPrivacy"
-                checked={isTeamPrivate}
-                onChange={handleSwitchChange}
-                className="toggle mx-2 border-primary bg-primary hover:border-secondary  hover:bg-secondary"
-              />
-              <span className="label-text  text-white">Public</span>
-            </label>
+    <>
+      <div className="modal modal-open">
+        <div className="modal-box shadow-xl rounded-lg">
+          <h3 className="font-bold text-xl mb-4 text-center">
+            Create a new Team:
+          </h3>
+          <div className="space-y-4 flex flex-col items-center">
+            <input
+              type="text"
+              value={teamName}
+              onChange={updateTeamName}
+              placeholder="Enter team name"
+              className="input  input-bordered input-primary w-3/4 input-sm"
+            />
+            <div className="flex justify-evenly items-center">
+              <label className="label cursor-pointer">
+                <span className="label-text text-white">Private</span>
+                <input
+                  type="checkbox"
+                  name="teamPrivacy"
+                  checked={isTeamPrivate}
+                  onChange={handleSwitchChange}
+                  className="toggle mx-2 border-primary bg-primary hover:border-secondary  hover:bg-secondary"
+                />
+                <span className="label-text  text-white">Public</span>
+              </label>
+            </div>
+            <div className="flex flex-col items-center">
+              {avatarPreview ? (
+                <img
+                  src={avatarPreview}
+                  alt="Team Avatar Preview"
+                  className="w-24 h-24 rounded-full mb-3 object-contain"
+                />
+              ) : (
+                <div className="w-24 h-24 bg-gray-700 rounded-full flex items-center justify-center mb-3">
+                  <span className="text-gray-400">No Avatar</span>
+                </div>
+              )}
+              <label className="btn btn-outline btn-primary btn-sm">
+                Upload Avatar
+                <input
+                  type="file"
+                  accept="image/*"
+                  className="hidden"
+                  onChange={handleAvatarChange}
+                />
+              </label>
+            </div>
           </div>
-          <div className="flex flex-col items-center">
-            {avatarPreview ? (
-              <img
-                src={avatarPreview}
-                alt="Team Avatar Preview"
-                className="w-24 h-24 rounded-full mb-3 object-contain"
-              />
-            ) : (
-              <div className="w-24 h-24 bg-gray-700 rounded-full flex items-center justify-center mb-3">
-                <span className="text-gray-400">No Avatar</span>
-              </div>
-            )}
-            <label className="btn btn-outline btn-primary btn-sm">
-              Upload Avatar
-              <input
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={handleAvatarChange}
-              />
-            </label>
+          <div className="modal-action justify-between">
+            <button className="btn btn-success" onClick={handleCreateTeam}>
+              Create
+            </button>
+            <button className="btn btn-error" onClick={closeModal}>
+              Cancel
+            </button>
           </div>
-        </div>
-        <div className="modal-action justify-between">
-          <button className="btn btn-success" onClick={handleCreateTeam}>
-            Create
-          </button>
-          <button className="btn btn-error" onClick={closeModal}>
-            Cancel
-          </button>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
