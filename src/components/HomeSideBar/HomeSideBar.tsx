f5c2ebbf3d0b7561b0c1232aed43abfc6e48f0a8
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-12-04 19:12:15 +0200

    update: HomeSideBar with loading state & spinner added

--- src/components/HomeSideBar/HomeSideBar.tsx
+++ src/components/HomeSideBar/HomeSideBar.tsx
@@ -6,24 +6,25 @@ import CreateTeam from "../../Views/ModalViews/CreateTeam/CreateTeam";
 import { onValue, ref } from "firebase/database";
 import { db } from "../../config/firebase-config";
 import { TeamAppContext } from "../../store/team.context";
-import { getTeamById, getTeams } from "../../services/team.service";
+import { getTeams } from "../../services/team.service";
 import { transformTeams } from "../../helper/helper";
 import { TeamModel } from "../../models/Team/TeamModel";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 const HomeSideBar: React.FC = (): JSX.Element => {
   const navigate = useNavigate();
   const [openModal, setOpenModal] = useState<boolean>(false);
+  const [loadingCreateTeam, setLoadingCreateTeam] = useState<boolean>(false);
   const [teams, setTeams] = useState<TeamModel[] | null>(null);
   const { setTeam } = useContext(TeamAppContext);
 
   const handleToPersonal = () => {
     navigate("/dms");
   };
-  const handleToTeam = async (teamId: string) => {
+  const handleToTeam = async (team: TeamModel) => {
     navigate("/home");
-    console.log(teamId);
+    console.log(team);
 
     try {
-      const team = await getTeamById(teamId);
       setTeam(team);
     } catch (error) {
       console.log("Error navigating to team view: ", error);
@@ -68,19 +69,28 @@ const HomeSideBar: React.FC = (): JSX.Element => {
         </div>
         <div className="list-of-teams flex flex-col space-y-2 pt-2 overflow-y-auto max-h-[calc(100vh-118px)] scrollbar-hide">
           {teams &&
-            teams.map((teamData: TeamModel) => {
+            teams.map((teamData: TeamModel, index: number) => {
               return (
-                <span
-                  key={teamData.teamId}
-                  onClick={() => handleToTeam(teamData.teamId)}
-                >
+                <span key={index} onClick={() => handleToTeam(teamData)}>
                   <TeamAvatarButton teamData={teamData} />
                 </span>
               );
             })}
         </div>
       </div>
-      {openModal && <CreateTeam closeModal={handleCreateTeam} />}
+      {loadingCreateTeam && (
+        <div className="absolute inset-0 flex items-center justify-center ">
+          <LoadingSpinner />
+        </div>
+      )}
+      {openModal && (
+        <>
+          <CreateTeam
+            closeModal={handleCreateTeam}
+            setLoading={val => setLoadingCreateTeam(val)}
+          />
+        </>
+      )}
     </>
   );
 };
