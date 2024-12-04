ec50fe7959f4800fd34d8058ad57e2cda80077c2
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-12-05 01:59:39 +0200

    add: Translations enum

---
+++ src/common/translations.ts
@@ -0,0 +1,8 @@
+export enum Translations {
+  BULGARIAN_EN = "Bulgarian",
+  BULGARIAN_BG = "Български",
+  ENGLISH_EN = "English",
+  ENGLISH_BG = "Английски",
+  LANGUAGE_PREFERENCE_EN = "Language Preference",
+  LANGUAGE_PREFERENCE_BG = "Езикови предпочитания",
+}
