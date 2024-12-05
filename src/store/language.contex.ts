258189eed36a791cec5702ae1463f753ad1b6709
doomdude7 <58223953+doomdude7@users.noreply.github.com>
2024-12-05 02:08:18 +0200

    add: language context & type

---
+++ src/store/language.contex.ts
@@ -0,0 +1,11 @@
+import React, { createContext, useState, useContext, ReactNode } from "react";
+import { Translations } from "../common/translations.ts";
+import { Language } from "../common/types/LanguageType.ts";
+
+type LanguageContextProps = {
+  language: Language;
+  setLanguage: (lang: Language) => void;
+  translate: (key: keyof typeof Translations) => string;
+};
+
+export const LanguageContext = createContext<LanguageContextProps | null>(null);
